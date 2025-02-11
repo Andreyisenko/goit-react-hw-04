b87dc4efc8e4d276bbd6aea1fb1720a63196a13f
Andrey Isenko <aisenko87@gmail.com>
2025-02-11 12:42:49 +0200

    add quer

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-let page = 1;
+// let page = 1;
 const YOUR_ACCESS_KEY = 'Hg1CpLsou6vsLxEalGGcMeVDU3n-0eTBhr2sOtrVw94';
-export const fetchArticles = async query => {
-  const params = new URLSearchParams({
-    client_id: YOUR_ACCESS_KEY,
-    query,
-    page,
-    per_page: 10,
-  });
+export const fetchArticles = async (query) => {
+  // const params = new URLSearchParams({
+  //   client_id: YOUR_ACCESS_KEY,
+  //   query,
+  //   page,
+  //   per_page: 20,
+  // });
   const response = await axios.get(
-    `https://api.unsplash.com/search/photos?${params}`
+    `https://api.unsplash.com/search/photos?client_id=${YOUR_ACCESS_KEY}&page=1&query=${query}`
+    // `https://api.unsplash.com/search/photos?client_id=${YOUR_ACCESS_KEY}&page=1&query=${query} ` 
   );
   console.log(response.data.results);
   return response.data.results;
